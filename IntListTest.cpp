93a68df9f0118b5bfa13208d3154fb200f9962eb
Vincent Regazzi <vincentregazzi@gmail.com>
2023-09-28 14:30:38 -0500

    add test text

--- IntListTest.cpp
+++ IntListTest.cpp
@@ -13,6 +13,7 @@ int main()
     cout << "At the beginning is_empty() returns: " << my_int_list.is_empty() << endl;
 
     // test push_head()
+    cout << "I will be pushing the head 3 times with values 7, 8, 9" << endl;
     my_int_list.push_head(7);
     my_int_list.push_head(8);
     my_int_list.push_head(9);
@@ -22,26 +23,30 @@ int main()
     cout << "\n";
 
     // test prepend()
+    cout << "I will be prepending 6" << endl;
     my_int_list.prepend(6);
     cout << "I expect the list to be 6, 9, 8, 7" << endl;
     my_int_list.print_list();
     cout << "\n";
 
     // test push_tail()
+    cout << "I will be pushing the tail with value 1" << endl;
     my_int_list.push_tail(1);
-    cout << "I expect the list to be 9, 8, 7, 1" << endl;
+    cout << "I expect the list to be 6, 9, 8, 7, 1" << endl;
     my_int_list.print_list();
     cout << "\n";
 
     // test append()
+    cout << "I will be appending 2" << endl;
     my_int_list.append(2);
-    cout << "I expect the list to be 9, 8, 7, 1, 2" << endl;
+    cout << "I expect the list to be 6, 9, 8, 7, 1, 2" << endl;
     my_int_list.print_list();
     cout << "\n";
 
     // test insert()
     // test insert in empty list
     IntList empty_list = IntList();
+    cout << "I will be inserting 1 into an empty list" << endl;
     empty_list.insert(0, 1);
     cout << "I expect the list to be 1" << endl;
     empty_list.print_list();
@@ -56,18 +61,21 @@ int main()
     new_list.push_head(5);
 
     // test insert at <= 0
+    cout << "I will be inserting 9 at index -1" << endl;
     new_list.insert(-1, 9);
     cout << "I expect the list to be 9, 5, 4, 3, 2, 1" << endl;
     new_list.print_list();
     cout << "\n";
 
     // test insert at >= size
+    cout << "I will be inserting 12 at index 12" << endl;
     new_list.insert(12, 12);
     cout << "I expect the list to be 9, 5, 4, 3, 2, 1, 12" << endl;
     new_list.print_list();
     cout << "\n";
 
     // test insert in middle
+    cout << "I will be inserting 8 at index 3" << endl;
     new_list.insert(3, 8);
     cout << "I expect the list to be 9, 5, 4, 8, 3, 2, 1, 12" << endl;
     new_list.print_list();
